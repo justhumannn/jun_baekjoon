1d92a923a0e1e78379f3d19b05b46196af66e72c
justhumannn <122255164+justhumannn@users.noreply.github.com>
2025-05-26 08:42:15 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n,k = map(int,input().split())
+result = 1
+for i in range(k):
+    result *= n
+    n -= 1
+divisor = 1
+for i in range(2,k+1):
+    divisor *= i
+print(result//divisor)
