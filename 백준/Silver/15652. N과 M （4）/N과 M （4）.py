04f246e06e8fb67700e1dfed3128b5ca3aa75f41
justhumannn <122255164+justhumannn@users.noreply.github.com>
2025-07-02 14:36:51 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,53 @@
+a,b = map(int,input().split())
+if b == 1:
+    for i in range(1,a+1):
+        print(i)
+elif b == 2:
+    for i in range(1,a+1):
+        for j in range(i,a+1):
+            print(i,j)
+elif b == 3:
+    for i in range(1,a+1):
+        for j in range(i,a+1):
+            for k in range(j,a+1):
+                print(i,j,k)
+elif b == 4:
+    for i in range(1,a+1):
+        for j in range(i,a+1):
+            for k in range(j,a+1):
+                for l in range(k,a+1):
+                    print(i,j,k,l)
+elif b == 5:
+    for i in range(1,a+1):
+        for j in range(i,a+1):
+            for k in range(j,a+1):
+                for l in range(k,a+1):
+                    for m in range(l,a+1):
+                        print(i,j,k,l,m)
+elif b == 6:
+    for i in range(1,a+1):
+        for j in range(i,a+1):
+            for k in range(j,a+1):
+                for l in range(k,a+1):
+                    for m in range(l,a+1):
+                        for n in range(m,a+1):
+                            print(i,j,k,l,m,n)
+elif b == 7:
+    for i in range(1,a+1):
+        for j in range(i,a+1):
+            for k in range(j,a+1):
+                for l in range(k,a+1):
+                    for m in range(l,a+1):
+                        for n in range(m,a+1):
+                            for o in range(n,a+1):
+                                print(i,j,k,l,m,n,o)
+else:
+    for i in range(1,a+1):
+        for j in range(i,a+1):
+            for k in range(j,a+1):
+                for l in range(k,a+1):
+                    for m in range(l,a+1):
+                        for n in range(m,a+1):
+                            for o in range(n,a+1):
+                                for p in range(o,a+1):
+                                    print(i,j,k,l,m,n,o,p)
