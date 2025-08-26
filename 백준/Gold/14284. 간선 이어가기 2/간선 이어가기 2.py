3d0a8d21cd679301dcda3ca6c897a43f68c542c0
justhumannn <122255164+justhumannn@users.noreply.github.com>
2025-08-26 15:40:03 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 196 ms, Memory: 54964 KB -BaekjoonHub

---
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.py
@@ -0,0 +1,24 @@
+import heapq
+import sys
+input = sys.stdin.readline
+n,m = map(int, input().split())
+graph = {i :[] for i in range(1,n+1)}
+for i in range(m):
+    a,b,c = map(int, input().split())
+    graph[a].append((b,c))
+    graph[b].append((a,c))
+start, end = map(int, input().split())
+sequence = [(0,start)]
+distance = [float('inf')] * (n+1)
+while sequence:
+    weight,node = heapq.heappop(sequence)
+    if weight > distance[node]:
+        continue
+    if node == end:
+        break
+    distance[node] = weight
+    for n1,w in graph[node]:
+        if distance[n1] > weight+w:
+            distance[n1] = weight+w
+            heapq.heappush(sequence, (distance[n1], n1))
+print(distance[end])
