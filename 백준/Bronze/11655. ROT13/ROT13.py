a9063823e990aba35da5b266740f759adfff8368
justhumannn <122255164+justhumannn@users.noreply.github.com>
2025-11-02 23:44:28 +0900

    [Bronze I] Title: ROT13, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,10 @@
+s = input()
+result = ""
+for ch in s:
+    if 'a' <= ch <= 'z':
+        result += chr((ord(ch) - ord('a') + 13) % 26 + ord('a'))
+    elif 'A' <= ch <= 'Z':
+        result += chr((ord(ch) - ord('A') + 13) % 26 + ord('A'))
+    else:
+        result += ch
+print(result)
