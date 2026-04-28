fdcb4c4cd022cd1dcebaf72a0a60774beb62e8c8
justhumannn <122255164+justhumannn@users.noreply.github.com>
2026-04-28 13:59:31 +0900

    [Diamond II] Title: Bulldozer, Time: 104 ms, Memory: 111556 KB, Score: 5 point -BaekjoonHub

--- 백준/Diamond/16783. Bulldozer/Bulldozer.py
+++ 백준/Diamond/16783. Bulldozer/Bulldozer.py
@@ -1,133 +1,157 @@
 import sys
-import math
-from functools import cmp_to_key
-from collections import defaultdict
+from math import gcd
 
-input_data = sys.stdin.read().split()
+input_data = sys.stdin.buffer.read().split()
 
 N = int(input_data[0])
+
 pts = []
 idx = 1
 for _ in range(N):
     pts.append((int(input_data[idx]), int(input_data[idx+1]), int(input_data[idx+2])))
     idx += 3
-    
+
 pts.sort(key=lambda x: (x[0], x[1]))
 
-X = [0] * N
-Y = [0] * N
-W = [0] * N
-for i in range(N):
-    X[i], Y[i], W[i] = pts[i]
-    
+X = [pt[0] for pt in pts]
+Y = [pt[1] for pt in pts]
+W = [pt[2] for pt in pts]
+
 pos = list(range(N))
 p = list(range(N))
 
 sz = 1
 while sz < N:
     sz <<= 1
-    
+
 tot = [0] * (sz << 1)
 lmax = [0] * (sz << 1)
 rmax = [0] * (sz << 1)
 mmax = [0] * (sz << 1)
 
-def update(idx, val):
-    idx += sz
-    tot[idx] = val
-    if val > 0:
-        lmax[idx] = rmax[idx] = mmax[idx] = val
-    else:
-        lmax[idx] = rmax[idx] = mmax[idx] = 0
-    
-    p_idx = idx >> 1
+def update(index, val):
+    index += sz
+    tot[index] = val
+    v = val if val > 0 else 0
+    lmax[index] = rmax[index] = mmax[index] = v
+
+    p_idx = index >> 1
     while p_idx:
         left = p_idx << 1
         right = left | 1
-        
-        t_l = tot[left]
-        t_r = tot[right]
+
+        t_l = tot[left]; t_r = tot[right]
         tot[p_idx] = t_l + t_r
-        
-        lm_l = lmax[left]
-        rm_r = rmax[right]
-        
-        cand1 = t_l + lmax[right]
-        lmax[p_idx] = lm_l if lm_l > cand1 else cand1
-        
-        cand2 = t_r + rmax[left]
-        rmax[p_idx] = rm_r if rm_r > cand2 else cand2
-        
-        m1 = mmax[left]
-        m2 = mmax[right]
-        m3 = rmax[left] + lmax[right]
-        
-        mx = m1
-        if m2 > mx: mx = m2
-        if m3 > mx: mx = m3
-        mmax[p_idx] = mx
-        
+
+        lm_l = lmax[left]; lr = t_l + lmax[right]
+        lmax[p_idx] = lm_l if lm_l > lr else lr
+
+        rm_r = rmax[right]; rl = t_r + rmax[left]
+        rmax[p_idx] = rm_r if rm_r > rl else rl
+
+        m1 = mmax[left]; m2 = mmax[right]; m3 = rmax[left] + lmax[right]
+        mx = m1 if m1 > m2 else m2
+        mmax[p_idx] = mx if mx > m3 else m3
+
         p_idx >>= 1
 
 for i in range(N):
     update(i, W[i])
-    
+
 max_ans = mmax[1]
 
-slopes = defaultdict(list)
+def normalize(dy, dx):
+    if dx < 0: dy, dx = -dy, -dx
+    elif dx == 0:
+        dy = 1
+    g = gcd(abs(dy), dx) if dx != 0 else abs(dy)
+    return (dy // g, dx // g) if g else (dy, dx)
+
+events = []
 for i in range(N):
     xi, yi = X[i], Y[i]
-    for j in range(i+1, N):
-        dx = X[j] - xi
+    for j in range(i + 1, N):
         dy = Y[j] - yi
-        g = math.gcd(dx, abs(dy))
-        slopes[(dx//g, dy//g)].append((i, j))
-        
-def cmp(k1, k2):
-    dx1, dy1 = k1
-    dx2, dy2 = k2
-    if dx1 == 0 and dx2 == 0: return 0
-    if dx1 == 0: return 1
-    if dx2 == 0: return -1
-    val = dy1 * dx2 - dy2 * dx1
-    if val < 0: return -1
-    if val > 0: return 1
-    return 0
-    
-sorted_slopes = sorted(slopes.keys(), key=cmp_to_key(cmp))
+        dx = X[j] - xi
+        if dx < 0: dy, dx = -dy, -dx
+        elif dx == 0: dy = 1
+        g = gcd(abs(dy), dx) if dx != 0 else abs(dy)
+        if g > 1: dy //= g; dx //= g
+        events.append((dy, dx, i, j))
 
-for slope in sorted_slopes:
-    dx, dy = slope
-    lines = {}
-    for u, v in slopes[slope]:
-        C = dy * X[u] - dx * Y[u]
-        if C not in lines:
-            lines[C] = [u, v]
-        else:
-            lines[C].append(u)
-            lines[C].append(v)
-            
-    for C, pts_list in lines.items():
-        unique_pts = list(set(pts_list))
-        min_p = N
-        max_p = -1
-        for x in unique_pts:
-            cur = pos[x]
-            if cur < min_p: min_p = cur
-            if cur > max_p: max_p = cur
-            
-        left, right = min_p, max_p
-        while left < right:
-            p[left], p[right] = p[right], p[left]
-            pos[p[left]] = left
-            pos[p[right]] = right
-            left += 1
-            right -= 1
-            
-        for idx in range(min_p, max_p + 1):
-            update(idx, W[p[idx]])
-            
+events.sort()
+
+n_events = len(events)
+idx = 0
+epoch = 0
+visited = [-1] * N
+
+while idx < n_events:
+    start_idx = idx
+    dy0, dx0 = events[idx][0], events[idx][1]
+
+    idx += 1
+    while idx < n_events and events[idx][0] == dy0 and events[idx][1] == dx0:
+        idx += 1
+
+    pair_count = idx - start_idx
+
+    if pair_count == 1:
+        u = events[start_idx][2]
+        v = events[start_idx][3]
+        left = pos[u]
+        right = pos[v]
+        if left > right:
+            left, right = right, left
+
+        p[left], p[right] = p[right], p[left]
+        pos[p[left]] = left
+        pos[p[right]] = right
+        update(left, W[p[left]])
+        update(right, W[p[right]])
+    else:
+        epoch += 1
+        unique_pts = []
+        for k in range(start_idx, idx):
+            u = events[k][2]
+            v = events[k][3]
+            if visited[u] != epoch:
+                visited[u] = epoch
+                unique_pts.append(u)
+            if visited[v] != epoch:
+                visited[v] = epoch
+                unique_pts.append(v)
+
+        unique_pts.sort(key=lambda x: pos[x])
+
+        i = 0
+        k_len = len(unique_pts)
+        while i < k_len:
+            start_pt = unique_pts[i]
+            C = X[start_pt] * dy0 - Y[start_pt] * dx0
+            j = i + 1
+            while j < k_len:
+                pt = unique_pts[j]
+                if X[pt] * dy0 - Y[pt] * dx0 == C:
+                    j += 1
+                else:
+                    break
+
+            left = pos[unique_pts[i]]
+            right = pos[unique_pts[j - 1]]
+
+            while left < right:
+                u = p[left]; v = p[right]
+                p[left] = v; p[right] = u
+                pos[u] = left; pos[v] = right
+                update(left, W[v])
+                update(right, W[u])
+                left += 1
+                right -= 1
+
+            i = j
+
     if mmax[1] > max_ans:
         max_ans = mmax[1]
-        
+
 print(max_ans)
